b76124a15162f7ff11c2a3168f06179b8685409c
Uwe Klotz <uwe.klotz@gmail.com>
2022-07-26 15:08:23 +0200

    Add an async variant of tasklet::capture_changes()

--- src/tasklet.rs
+++ src/tasklet.rs
@@ -3,6 +3,8 @@
 
 //! Tasklets for processing observed values.
 
+use std::future::Future;
+
 use crate::Subscriber;
 
 /// Observe a shared value.
@@ -11,6 +13,9 @@ use crate::Subscriber;
 /// is started and then again after each change. The shared value is
 /// locked during the invocation. It must return `true` to continue
 /// and `false` to abort the task.
+///
+/// No `_async` variant of this function could be provided, because
+/// holding locks across yield points is not permitted.
 pub async fn observe_changes<T>(
     mut subscriber: Subscriber<T>,
     mut on_changed: impl FnMut(&T) -> bool,
@@ -27,7 +32,7 @@ pub async fn observe_changes<T>(
     }
 }
 
-/// Capture changes by observing a shared value.
+/// Capture changes while observing a shared value.
 ///
 /// The `capture` closure transforms a borrowed reference of the
 /// observed value into an owned instance of the captured value.
@@ -52,6 +57,39 @@ pub async fn capture_changes<S, T>(
     let mut value = capture(&*subscriber.read_ack());
     loop {
         if !on_changed(&value) {
+            // Aborted by consumer
+            return;
+        }
+        loop {
+            if subscriber.changed().await.is_err() {
+                // Publisher has disappeared
+                return;
+            }
+            let new_value = subscriber.read_ack();
+            if has_changed(&value, &*new_value) {
+                value = capture(&*new_value);
+                // Exit inner loop for sending a notification
+                break;
+            }
+        }
+    }
+}
+
+/// Capture changes asynchronously while observing a shared value.
+///
+/// Same as [`capture_changes()`] with the only difference that
+/// the `on_changed` closure returns a future with the result.
+pub async fn capture_changes_async<S, T, F>(
+    mut subscriber: Subscriber<S>,
+    mut capture: impl FnMut(&S) -> T,
+    mut has_changed: impl FnMut(&T, &S) -> bool,
+    mut on_changed: impl FnMut(&T) -> F,
+) where
+    F: Future<Output = bool>,
+{
+    let mut value = capture(&*subscriber.read_ack());
+    loop {
+        if !on_changed(&value).await {
             // Aborted by consumer
             return;
         }
